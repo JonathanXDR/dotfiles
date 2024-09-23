260e838ba0b4abb338d3459d402df03a48dd5820
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-09-23 18:54:13 +0200

    Update vars.sh

--- .shell/vars.sh
+++ .shell/vars.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
 
-export PROXY_PROTOCOL="http"
-export PROXY_HOST="aproxy.corproot.net"
-export PROXY_PORT="8080"
-export NOPROXY="127.0.0.1/32,.swisscom.com,.swisscom.ch,.corproot.net,.sharedtcs.net,.docker.internal,.eks.amazonaws.com"
-export NO_PROXY_DNS="1.1.1.1,8.8.8.8"
-export PROXY_DNS="193.47.226.200,193.47.226.40"
-
-export AWS_PROXY_PROTOCOL=""
-export AWS_PROXY_HOST=""
-export AWS_PROXY_PORT=""
-export AWS_NOPROXY=""
-
-export AWS_CLUSTER_NAME=
-export AWS_REGION=
-
-export JENKINS_USER_ID=""
-export JENKINS_API_TOKEN=""
-export JENKINS_URL=""
-
-export ENTERPRISE_DOMAIN=""
-export ALWAYS_PROXY_PROBE=""
-
-export K3S_KUBECONFIG_MODE="644"
+if [ -f .env ]; then
+  set -o allexport
+  source .env
+  set +o allexport
+fi
 
 export BUN_INSTALL="${HOME}/.bun"
 export NVM_DIR="${HOME}/.nvm"
-
 export EDITOR="nano"
 export RESOLF='/etc/resolv.conf'
 
 export PATH="${BUN_INSTALL}/bin:${PATH}"
 export PATH="${PATH}:${HOME}/.rvm/bin"
-export PATH=~/.console-ninja/.bin:$PATH
+export PATH="${HOME}/.console-ninja/.bin:${PATH}"
 export PATH="/Library/Frameworks/Python.framework/Versions/3.12/bin:${PATH}"
-export PATH="${PATH}}:/Users/jonathan/Library/Application Support/JetBrains/Toolbox/scripts"
+export PATH="${PATH}:/Users/jonathan/Library/Application Support/JetBrains/Toolbox/scripts"
